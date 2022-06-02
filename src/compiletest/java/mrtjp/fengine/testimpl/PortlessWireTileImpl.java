c04ce14cb35a1d3d9e0c109299b588b470fb891f
MrTJP <mrtjp@icloud.com>
2022-06-02 02:40:48 -0400

    test: verify wire inputs from manifest

--- src/compiletest/java/mrtjp/fengine/testimpl/PortlessWireTileImpl.java
+++ src/compiletest/java/mrtjp/fengine/testimpl/PortlessWireTileImpl.java
@@ -1,13 +1,30 @@
 package mrtjp.fengine.testimpl;
 
+import mrtjp.fengine.api.IPathFinder;
+import mrtjp.fengine.api.IPathFinderManifest;
 import mrtjp.fengine.tiles.FEPortlessWireTile;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class PortlessWireTileImpl extends FEPortlessWireTile {
 
     private final int connMask;
 
+    public final Set<Integer> inputRegisters = new HashSet<>();
+
     public PortlessWireTileImpl(int connMask) {
         this.connMask = connMask;
+    }
+
+    @Override
+    public void locate(IPathFinder pathFinder) {
+        inputRegisters.clear();
+    }
+
+    @Override
+    public void searchManifest(IPathFinderManifest manifest) {
+        inputRegisters.addAll(manifest.getOutputRegisters()); // Found output registers feed into this wire as inputs
     }
 
     //@formatter:off
